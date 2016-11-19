dabedb8aa49b9d77c281428d7f45a94b38a68192
Erin Eka Citra <erin8215@students.amikom.ac.id>
2016-11-19 21:27:40 +0700

    Update view_sisw.php

--- view_sisw.php
+++ view_sisw.php
@@ -21,6 +21,27 @@ Hasil = <?php echo $hasil; ?>
 <td>kode</td>
 <td>judul</td>
 </tr>
+<?php 
+foreach ($tabelsiswa as $baris){
+ ?>
+<tr>
+<td> <?php echo $baris->nis; ?>&nbsp;</td>
+<td> <?php echo $baris->nama; ?>&nbsp;</td>
+<td> <?php echo $baris->alamat; ?>&nbsp;</td>
+<td> <?php echo $baris->lahir; ?>&nbsp;</td>
+<td> <?php echo $baris->nilai; ?>&nbsp;</td>
+<td> <?php echo $baris->foto; ?>&nbsp;</td>
+<td> <?php echo $baris->jk; ?>&nbsp;</td>
+<td> <?php echo $baris->pendidikan; ?>&nbsp;</td>
+<td> <?php echo $baris->status; ?>&nbsp;</td>
+<td> <?php echo $baris->kode; ?>&nbsp;</td>
+<td> <?php echo $baris->judul; ?>&nbsp;</td>
+<td> <img src="<?php echo base_url(); ?>uploads/<?php echo $baris->foto; ?>" width="100"> </td>
+<td><a href="<?php echo site_url("siswa/edit/".$baris->nis); ?>">Edit</a> &nbsp; <a href="<?php echo site_url("siswa/del/".$baris->nis);  ?>">Del</a></td>
+</tr>
+<?php 
+}
+ ?>
 </table>
 </body>
 </html>
